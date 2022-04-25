a67594fcb2d45d480faf58de5e3654b1c4548f76
Evgen Kozenkov <e.kozenkov@kernelics.com>
2022-04-25 11:11:39 +0300

    feature/create card

---
+++ src/constants/Cards.js
@@ -0,0 +1,10 @@
+export const cards = [
+  {
+    imageUrl: 'http://artismedia.by/blog/wp-content/uploads/2018/05/in-blog2-1.png',
+    imageAlt: 'image',
+    title: 'Wild life',
+    text: 'dasaddajfhjhfaskh hsjafhafhjsfhka',
+    author: 'Admin',
+    date: '25-04-2022'
+  }
+]
