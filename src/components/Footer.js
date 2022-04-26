c6857cbcaeda7847f629adb4f35d0bac7d98cc33
Evgen Kozenkov <e.kozenkov@kernelics.com>
2022-04-26 11:43:03 +0300

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
 import { Link } from 'gatsby'
+
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+import logo from '../assets/images/logo.svg'
 
 export const Footer = () => {
   return (
     <footer className='footer'>
-<     Typography align="center">
+      <div className='footer_mainPart'>
+        <Link to='/'>
+          <img src={logo} alt="my blog logo"/>
+        </Link>
+        <div>
+          <Link to='/' className="nav-link" activeClassName='active-link'>
+            home
+          </Link>
+          <Link to='/blog' className="nav-link" activeClassName='active-link'>
+            blog
+          </Link>
+          <Link to='/about' className="nav-link" activeClassName='active-link'>
+            about
+          </Link>
+          <Link to='/contact' className="nav-link" activeClassName='active-link'>
+            contact
+          </Link>
+        </div>
+        <form className='footer_form'>
+          <label for="email">
+            Subscribe here and get the latest travel tips  and my insider secrets!
+          </label>
+          <input type='email' id="email" placeholder='Email' pattern=".+@globex\.com" size="30" required/>
+          <Button variant="contained">Submit</Button>
+        </form>
+      </div>
+      <Typography align="center">
         {'Copyright © '}
         <Link to="/">
           My blog
