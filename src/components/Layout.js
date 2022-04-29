664185d1f70059dde80921f694cbe8d24defcd82
Evgen Kozenkov <e.kozenkov@kernelics.com>
2022-04-29 09:54:05 +0300

    add metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "normalize.css"
+
 import { Footer } from './Footer'
 import { Header } from './Header'
 
-import "normalize.css"
 import "../assets/css/main.css"
 
 export const Layout = ({children}) => {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,10 +3,12 @@ import React from 'react'
 import { Layout } from '../components/Layout'
 import { ImgMediaCard } from '../components/ImgMediaCard'
 import { cards } from '../constants/Cards'
+import { Seo } from '../components/Seo'
 
 const Blog = () => {
   return (
     <Layout>
+      <Seo title='Blog'/>
       <section className='blogpage'>
         <h1>Blog</h1>
         <div className='blogpage_cards'>
