07a69909d156d5a190b126eb4df1c390c7781f8b
Evgen Kozenkov <e.kozenkov@kernelics.com>
2022-05-05 13:40:27 +0300

    add validation

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
@@ -6,29 +6,109 @@ import Button from '@mui/material/Button'
 import * as styles from '../assets/css/contactForm.module.css'
 
 export const ContactForm = () => {
+
+  const [firstName, setFirstName] = useState(''); 
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastName, setLastName] = useState(''); 
+  const [lastNameError, setLastNameError] = useState(false);
+  const [telNumber, setTelNumber] = useState(''); 
+  const [telNumberError, setTelNumberError] = useState(false);
+  const [mail, setMail] = useState(''); 
+  const [mailError, setMailError] = useState(false);
+  const [message, setMessage] = useState(''); 
+  const [messageError, setMessageError] = useState(false);
+
+  const submitHandler = (e) => {
+    if (!firstNameError && !lastNameError && !telNumberError && !mailError  && !messageError) {
+      alert(`Thanks ${firstName, lastName} for your message`);
+      setFirstName('');
+      setLastName('');
+      setTelNumber('');
+      setMail('');
+      setMessage('');
+    }
+  }
+
+  const firstNameHandler = (e) => {
+    setFirstName(e.target.value);
+    e.target.value.length > 10 ? setFirstNameError(true) : setFirstNameError(false);
+  }
+
+  const lastNameHandler = (e) => {
+    setLastName(e.target.value);
+    e.target.value.length > 30 ? setLastNameError(true) : setLastNameError(false);
+  }
+
+  const mailHandler = (e) => {
+    setMail(e.target.value);
+    let result = e.target.value.match('^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$');
+    result === null && e.target.value ? setMailError(true) : setMailError(false);
+  }
+
+  const telNumberHandler = (e) => {
+    setTelNumber(e.target.value);
+    let result = e.target.value.match('/\d/gm');
+    result === null && e.target.value.length!==11 && e.target.value ? setTelNumberError(true) : setTelNumberError(false);
+  }
+
+  const messageHandler = (e) => {
+    setMessage(e.target.value);
+    e.target.value.length > 140 ? setMessageError(true) : setMessageError(false);
+  }
+
   return (
     <div className={styles.contactpage_socials}>
       <h3>Or just drop me a line here</h3>
-      <form className={styles.contactpage_form}>
+      <form className={styles.contactpage_form} onSubmit={submitHandler}>
         <div className={styles.contactpage_inputs}>
-          <TextField id="firstName" label="First Name" variant="outlined" />
-          <TextField id="lastName" label="Last Name" variant="outlined" />
+          <TextField 
+            error={firstNameError}
+            id="firstName" 
+            label="First Name" 
+            variant="outlined" 
+            onChange={(e) => firstNameHandler(e)}
+            helperText={firstNameError ? "Max Length 10 symbols" : ""}
+          />
+          <TextField 
+            error={lastNameError}
+            id="lastName" 
+            label="Last Name" 
+            variant="outlined"
+            onChange={(e) => lastNameHandler(e)}
+            helperText={lastNameError ? "Max Length 30 symbols" : ""}/>
         </div>
         <div className={styles.contactpage_inputs}>
-          <TextField id="email" label="Email" variant="outlined" required/>
-          <TextField id="phone" label="Phone" variant="outlined" />
+          <TextField 
+            error={mailError}
+            id="email" 
+            label="Email" 
+            variant="outlined" 
+            onChange={(e) => mailHandler(e)}
+            helperText={mailError ? "Pattern: xxx@yyyy.zzz" : ""}
+            required
+          />
+          <TextField 
+            error={telNumberError}
+            id="phone" 
+            label="Phone" 
+            variant="outlined"
+            onChange={(e) => telNumberHandler(e)}
+            helperText={telNumberError ? "Pattern: ccccxxxxxxx" : ""}
+          />
         </div>
         <div className={styles.contactpage_textfieldWrapper}>
           <TextField
+            error={messageError}
             className={styles.contactpage_textfield}
             id="message"
             label="Message"
             multiline
             rows={4}
-            defaultValue="Hi"
+            onChange={(e) => messageHandler(e)}
+            helperText={messageError ? "Max length 140 symbols" : ""}
           />
         </div>
-        <Button variant="contained" className={styles.contactpage_button}>Send</Button>
+        <Button type='submit' variant="contained" className={styles.contactpage_button}>Send</Button>
       </form>
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'gatsby'
 
+import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 
@@ -8,6 +9,24 @@ import logo from '../assets/images/logo.svg'
 import * as styles from '../assets/css/footer.module.css'
 
 export const Footer = () => {
+
+  const [mail, setMail] = useState(''); 
+  const [mailError, setMailError] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!mailError && mail) {
+      alert(`Thanks for your message`);
+      window.location.reload();
+    }
+  }
+
+  const mailHandler = (e) => {
+    setMail(e.target.value);
+    let result = e.target.value.match('^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$');
+    result === null && e.target.value ? setMailError(true) : setMailError(false);
+  }
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footer_mainPart}>
@@ -28,12 +47,19 @@ export const Footer = () => {
             contact
           </Link>
         </div>
-        <form className={styles.footer_form}>
+        <form onSubmit={submitHandler} className={styles.footer_form}>
           <label htmlFor="email">
             Subscribe here and get the latest travel tips  and my insider secrets!
           </label>
-          <input type='email' id="email" placeholder='Email' pattern=".+@globex\.com" size="30" required/>
-          <Button variant="contained">Submit</Button>
+          <TextField
+            error={mailError}
+            id="email" 
+            label="Email" 
+            variant="filled" 
+            onChange={(e) => mailHandler(e)}
+            helperText={mailError ? "Pattern: xxx@yyyy.zzz" : ""}
+          />
+          <Button type='submit' variant="contained">Submit</Button>
         </form>
       </div>
       <Typography align="center">
